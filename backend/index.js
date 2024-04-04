80f23ecc7dc95a001ad4ff25ca02696fdc3d0c0b
SARB <mrsinghbusiness05@gmail.com>
2024-04-04 19:15:22 +0530

    frontend/backend connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,29 @@
 import express from 'express' ;
+import { Server } from 'socket.io';
+import http from 'http';
+import cors from 'cors';
 
 const app = express();
+const server = http.createServer(app);
 
-const PORT= 3000;
+const io = new Server(server, {
+    cors : {
+        origin: 'http://localhost:5173',
+        methods: ['GET','POST'],
+        credentials :true
+    }
+});
 
-app.listen(PORT,() => {
-    console.log(` Listening to PORT ${PORT}  Bro `); 
+app.use(cors());
+//io = circuit 
+
+
+io.on('connection' , (socket) => {
+    console.log('Connection created =',socket.id);
+})
+
+const PORT = 3000;
+
+server.listen(PORT,() => {
+    console.log(` Server / PORT Listening ${PORT}  Bro `); 
 })
